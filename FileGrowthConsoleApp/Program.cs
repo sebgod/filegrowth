9b4eabbf525074eeebc861b6c263a02ddf7d051b
Sebastian Godelet <sebastian.godelet@outlook.com>
2018-11-08 21:07:38 +0000

    add console app

---
+++ FileGrowthConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileGrowthConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
